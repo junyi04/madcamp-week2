ce505a53f5cb96f10c44d2f5f9e3183a3554bd59
junyi04 <amber542825@gmail.com>
2026-01-15 21:17:31 +0900

    Feat: Swagger 문서 생성 및 경로 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정 객체 생성
+  const config = new DocumentBuilder()
+    .setTitle("Code Galaxy API")
+    .setDescription("Github 데이터를 이용한 API 문서")
+    .setVersion('1.0')
+    .addTag('Galaxy')
+    .build();
+
+  // Swagger 문서 생성 및 경로 설정
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: http://localhost:3000/api`);
 }
 bootstrap();
