599f33f0c7faf4eadab2494fbb7d0bd9f5b4286d
junyi04 <amber542825@gmail.com>
2026-01-16 22:58:32 +0900

    Feat: Star 좌표 추가

---
+++ src/universe/universe.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UniverseController } from './universe.controller';
+import { UniverseService } from './universe.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  controllers: [UniverseController],
+  providers: [UniverseService, PrismaService],
+})
+export class UniverseModule {}
